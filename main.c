51c8637c85fd560948ca950efe8fc28ba4eeeada
mabr3 <miguel.b.rodrigues@tecnico.ulisboa.pt>
2017-09-29 15:56:55 +0100

    Commit of initial files.

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "file.h"
+#include "struct.h"
+
+
+int main(int argc, char * argv){
+
+	if(argc != 1){
+		printf("Wrong number of arguments. Try again.\n");
+		exit(0);
+	}
+
+
+}
